fc2e7161211a66568ff047c061de94925807c515
freen <934415751@qq.com>
2023-08-09 22:32:43 +0800

    add 开车 add线程安全

--- src/main/kotlin/cn/chahuyun/config/DriverCarEventConfig.kt
+++ src/main/kotlin/cn/chahuyun/config/DriverCarEventConfig.kt
@@ -1,11 +1,14 @@
 package cn.chahuyun.config
 
+import kotlinx.serialization.Serializable
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
+import java.util.concurrent.ConcurrentHashMap
 
 object DriverCarEventConfig : AutoSavePluginConfig("DriverCarEventConfig") {
 
     @ValueDescription("开车列表")
-    var driverCar: Map<Long, List<CarDetail>> by value()
+    @Serializable
+    var driverCar: ConcurrentHashMap<Long, List<CarDetail>> =  ConcurrentHashMap();
 }
