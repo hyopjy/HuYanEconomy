8ed79269a6d0921d6b47a28220179c6ce4d9d71c
zhangyi <zhangyi@hummerdata.com>
2023-08-18 10:43:40 +0800

    ByteBuddy 整合切面

---
+++ src/main/java/cn/chahuyun/economy/aop/Log.java
@@ -0,0 +1,8 @@
+package cn.chahuyun.economy.aop;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Log {
+}
---
+++ src/main/java/cn/chahuyun/economy/aop/Service.java
@@ -0,0 +1,14 @@
+package cn.chahuyun.economy.aop;
+
+public class Service {
+    @Log
+    public int foo(int value) {
+        System.out.println("foo: " + value);
+        return value;
+    }
+
+    public int bar(int value) {
+        System.out.println("bar: " + value);
+        return value;
+    }
+}
