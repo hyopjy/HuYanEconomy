8928167f9ec4c13d954f2456b56cf3ab4ed5ca2f
steve <934415751@qq.com>
2023-08-23 11:38:55 +0800

    add 福丽恩

--- src/main/java/cn/chahuyun/economy/command/FreenFish.java
+++ src/main/java/cn/chahuyun/economy/command/FreenFish.java
@@ -6,14 +6,14 @@ import cn.chahuyun.economy.utils.EconomyUtil;
 import cn.chahuyun.economy.utils.MessageUtil;
 import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.contact.User;
-import net.mamoe.mirai.message.data.*;
+import net.mamoe.mirai.message.data.Message;
+import net.mamoe.mirai.message.data.MessageChainBuilder;
+import net.mamoe.mirai.message.data.PlainText;
+import net.mamoe.mirai.message.data.QuoteReply;
 
 import java.util.regex.Pattern;
 
-/**
- * 《小喇叭鱼的故事》
- */
-public class LittleTrumpetFishStory extends AbstractPropUsage {
+public class FreenFish extends AbstractPropUsage {
     @Override
     public boolean checkOrder() {
         String no = PropsType.getNo(propsCard.getCode());
@@ -33,14 +33,14 @@ public class LittleTrumpetFishStory extends AbstractPropUsage {
         User sender = event.getSender();
 
         // 自己获得
-        EconomyUtil.plusMoneyToUser(sender, 13140);
+        EconomyUtil.plusMoneyToUser(sender, 808);
 
         subject.sendMessage(new MessageChainBuilder().append(new QuoteReply(event.getMessage()))
                 .append(propsCard.getName() + "使用成功").append("\r\n")
-                .append("成功获得13140币币")
+                .append("成功获得808币币")
                 .build());
 
-        Message m = new PlainText("开始讲述《小喇叭鱼的故事》的故事 \r\n");
+        Message m = new PlainText("开始讲述《福丽恩》的故事 \r\n");
         m = m.plus(propsCard.getDescription());
         subject.sendMessage(m);
     }
