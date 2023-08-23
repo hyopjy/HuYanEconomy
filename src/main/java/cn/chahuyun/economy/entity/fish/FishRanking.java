420c88124575e8981cbf281f8be458f12c2d6cef
steve <934415751@qq.com>
2023-08-23 17:50:01 +0800

    add 修复鱼池多条数据问题

--- src/main/java/cn/chahuyun/economy/entity/fish/FishRanking.java
+++ src/main/java/cn/chahuyun/economy/entity/fish/FishRanking.java
@@ -57,7 +57,8 @@ public class FishRanking implements Serializable {
      * 钓起来的鱼
      */
     @ManyToOne(targetEntity = Fish.class, fetch = FetchType.EAGER)
-    @JoinColumn(name = "FishId",referencedColumnName = "id")
+     @JoinColumn(name = "FishId",referencedColumnName = "id")
+    //@JoinColumn(name = "FishId")
     private Fish fish;
     /**
      * 钓起来的鱼塘
