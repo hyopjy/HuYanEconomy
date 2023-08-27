8cea0b62b8a8f33713d0a09a767b111bff9bf68a
steve <934415751@qq.com>
2023-08-27 23:25:34 +0800

    add 修复刷新钓鱼bug

--- src/main/java/cn/chahuyun/economy/redis/RedissonConfig.java
+++ src/main/java/cn/chahuyun/economy/redis/RedissonConfig.java
@@ -1,24 +1,22 @@
-//package cn.chahuyun.economy.redis;
-//
-//import org.redisson.Redisson;
-//import org.redisson.api.RedissonClient;
-//import org.redisson.config.Config;
-//
-//public class RedissonConfig {
-//    private static Config config = new Config();
-//
-//    //声明redisso对象
-//    private static Redisson redisson = null;
-//    static{
-//        config.useSingleServer().setAddress("redis://8.140.194.225:6739").setPingConnectionInterval(10000);
-//        //得到redisson对象
-//        redisson = (Redisson) Redisson.create(config);
-//
-//    }
-//    //获取redisson对象的方法
-//    public static Redisson getRedisson(){
-//        return redisson;
-//    }
-//
-//
-//}
+package cn.chahuyun.economy.redis;
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+public class RedissonConfig {
+     private static Config config = new Config();
+
+    //声明redisso对象
+    private static Redisson redisson = null;
+    static{
+        config.useSingleServer()
+                .setAddress("redis://8.140.194.225:6379")
+                .setPassword("zyjy110.");
+        //得到redisson对象
+        redisson = (Redisson) Redisson.create(config);
+
+    }
+    //获取redisson对象的方法
+    public static Redisson getRedisson(){
+        return redisson;
+    }
+
+}
