35bf77924278c7cb4d5fd7d24b414305f5aa087a
zhangyi <zhangyi@hummerdata.com>
2023-09-04 14:10:15 +0800

    add 自定义一日头衔延期策略

--- src/main/java/cn/chahuyun/economy/redis/RedisUtils.java
+++ src/main/java/cn/chahuyun/economy/redis/RedisUtils.java
@@ -1,13 +1,17 @@
 package cn.chahuyun.economy.redis;
 
+import cn.chahuyun.economy.HuYanEconomy;
 import cn.chahuyun.economy.constant.RedisKeyConstant;
-import cn.chahuyun.economy.dto.SpecialTitleDto;
 import cn.chahuyun.economy.utils.Log;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.json.JSONUtil;
+import net.mamoe.mirai.contact.Group;
+import net.mamoe.mirai.contact.NormalMember;
+import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.message.data.Message;
 import org.redisson.api.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +20,9 @@ import java.util.concurrent.TimeUnit;
 
 public class RedisUtils {
     // http://www.rply.cn/news/43007.html
+
+    private static String SPECIAL_TITLE_ONE_DAY_QUEUE = "SpecialTitleOneDayQueue";
+
     public static void setKeyString(String key, String value) {
         RBucket<String> bucket = RedissonConfig.getRedisson().getBucket(key);
         bucket.set(value);
@@ -24,6 +31,7 @@ public class RedisUtils {
     public static String getKeyString(String key) {
         return (String) RedissonConfig.getRedisson().getBucket(key).get();
     }
+
 
     public static RBloomFilter<Long> getFishSignBloomFilter(Long groupId,String prop) {
         String key = "fish:sign:" + prop + RedisKeyConstant.COLON_SPILT + groupId;
@@ -75,114 +83,60 @@ public class RedisUtils {
        return RedissonConfig.getRedisson().getLock(RedisKeyConstant.FISH_LOCK_KEY + groupId + RedisKeyConstant.COLON_SPILT + userId);
     }
 
-    /**
-     * 添加对象进延时队列
-     * @param putInData 添加数据
-     * @param delay     延时时间
-     * @param timeUnit  时间单位
-     * @param queueName 队列名称
-     * @param <T>
-     */
-    private static  <T>  void addQueue(T putInData,long delay, TimeUnit timeUnit, String queueName){
-        Log.info("添加延迟队列,监听名称:"+queueName+",时间:"+delay+",时间单位:"+timeUnit+",内容:"+putInData+"");
-        RBlockingQueue<T> blockingFairQueue = RedissonConfig.getRedisson().getBlockingQueue(queueName);
-        RDelayedQueue<T> delayedQueue = RedissonConfig.getRedisson().getDelayedQueue(blockingFairQueue);
-        delayedQueue.offer(putInData, delay, timeUnit);
-    }
 
-    /**
-     * 添加队列-秒
-     *
-     * @param t     DTO传输类
-     * @param delay 时间数量
-     * @param <T>   泛型
-     */
-    public static  <T> void addQueueSeconds(T t, long delay, Class<? extends RedisDelayedQueueListener> clazz) {
-        addQueue(t, delay, TimeUnit.SECONDS, clazz.getName().replace(".", RedisKeyConstant.COLON_SPILT));
-    }
-
-    /**
-     * 添加队列-分
-     *
-     * @param t     DTO传输类
-     * @param delay 时间数量
-     * @param <T>   泛型
-     */
-    public static  <T> void addQueueMinutes(T t, long delay, Class<? extends RedisDelayedQueueListener> clazz) {
-        addQueue(t, delay, TimeUnit.MINUTES, clazz.getName().replace(".", RedisKeyConstant.COLON_SPILT));
-    }
-
-    /**
-     * 添加队列-时
-     *
-     * @param t     DTO传输类
-     * @param delay 时间数量
-     * @param <T>   泛型
-     */
-    public static  <T> void addQueueHours(T t, long delay, Class<? extends RedisDelayedQueueListener> clazz) {
-        addQueue(t, delay, TimeUnit.HOURS, clazz.getName().replace(".", RedisKeyConstant.COLON_SPILT));
-    }
-    /**
-     * 添加队列-天
-     *
-     * @param t     DTO传输类
-     * @param delay 时间数量
-     * @param <T>   泛型
-     */
-    public static  <T> void addQueueDays(T t, long delay, Class<? extends RedisDelayedQueueListener> clazz) {
-        addQueue(t, delay, TimeUnit.DAYS, clazz.getName().replace(".", RedisKeyConstant.COLON_SPILT));
-    }
-
-    /**
-     * 删除指定队列中的消息
-     *
-     */
-    public static boolean removeDelayedQueue(Object putInData, String queueName) {
-        if (StrUtil.isBlank(queueName) || Objects.isNull(putInData)) {
-            return false;
-        }
-        RBlockingDeque<Object> blockingDeque = RedissonConfig.getRedisson().getBlockingDeque(queueName);
-        RDelayedQueue<Object> delayedQueue = RedissonConfig.getRedisson().getDelayedQueue(blockingDeque);
-        boolean flag = delayedQueue.remove(putInData);
-        //delayedQueue.destroy();
-        return flag;
-    }
-
-
-    public static void initDelayedQueueTask(){
-        // https://www.dianjilingqu.com/635557.html
-//        Map<String, RedisDelayedQueueListener> map = applicationContext.getBeansOfType(RedisDelayedQueueListener.class);
-//        for (Map.Entry<String, RedisDelayedQueueListener> taskEventListenerEntry : map.entrySet()) {
-//            String listenerName = taskEventListenerEntry.getValue().getClass().getName();
-//            startThread(listenerName, taskEventListenerEntry.getValue());
-//        }
-
-        String listenerName = SpecialTitleOneDayExpirationListener.class.getName();
-        listenerName = listenerName.replace(".",RedisKeyConstant.COLON_SPILT);
-        startThread(listenerName, new SpecialTitleOneDayExpirationListener<SpecialTitleDto>());
-    }
-
-    /**
-     * 启动线程获取队列
-     * @param queueName 队列名称
-     * @param redisDelayedQueueListener 任务回调监听
-     */
-    private static <T> void startThread(String queueName, RedisDelayedQueueListener redisDelayedQueueListener) {
-        RBlockingQueue<T> blockingFairQueue = RedissonConfig.getRedisson().getBlockingQueue(queueName);
-        //由于此线程需要常驻，可以新建线程，不用交给线程池管理
+    public static void initDelay() {
         Thread thread = new Thread(() -> {
-            Log.info("启动监听队列线程" + queueName);
-            while (true) {
-                try {
-                    T t = blockingFairQueue.take();
-                    Log.info("监听队列线程" + queueName + ",获取到值:" + JSONUtil.toJsonStr(t) + "");
-                    redisDelayedQueueListener.invoke(t);
-                } catch (Exception e) {
-                    Log.error("监听队列线程错误," + e);
-                }
-            }
+            initDelayedQueueTask();
         });
-        thread.setName(queueName);
         thread.start();
     }
+
+    public static void initDelayedQueueTask(){
+        RBlockingDeque<String> blockingDeque = RedissonConfig.getRedisson().getBlockingDeque(SPECIAL_TITLE_ONE_DAY_QUEUE);
+        // 注意虽然delayedQueue在这个方法里面没有用到，但是这行代码也是必不可少的。
+        RDelayedQueue<String> delayedQueue = RedissonConfig.getRedisson().getDelayedQueue(blockingDeque);
+        while (true) {
+            String orderId = null;
+            try {
+                orderId = blockingDeque.take();
+            } catch (Exception e) {
+                Log.error("initDelayedQueueTask 发生异常！");
+                e.printStackTrace();
+                continue;
+            }
+            if (StrUtil.isBlank(orderId)) {
+                continue;
+            }
+            Log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "延时队列收到:" + orderId);
+            String[] orderIds = orderId.split("-");
+            Long groupId = Long.valueOf(orderIds[0]);
+            Long userId  = Long.valueOf(orderIds[1]);
+            Group group = HuYanEconomy.INSTANCE.bot.getGroup(groupId);
+            if(Objects.isNull(group)){
+                continue;
+            }
+            NormalMember normalMember = group.get(userId);
+            if(Objects.isNull(normalMember)){
+                continue;
+            }
+            normalMember.setSpecialTitle("");
+            Message m = new At(userId).plus("自定义一日title已过期");
+            group.sendMessage(m);
+        }
+
+    }
+    public static void addTaskToDelayQueue(String orderId, int delay,TimeUnit timeUnit) {
+        // RBlockingDeque的实现类为:new RedissonBlockingDeque
+        RBlockingDeque<String> blockingDeque = RedissonConfig.getRedisson().getBlockingDeque(SPECIAL_TITLE_ONE_DAY_QUEUE);
+        // RDelayedQueue的实现类为:new RedissonDelayedQueue
+        RDelayedQueue<String> delayedQueue = RedissonConfig.getRedisson().getDelayedQueue(blockingDeque);
+        if(delayedQueue.contains(orderId)){
+            blockingDeque.remove(orderId);
+            delayedQueue.remove(orderId);
+        }
+
+        Log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + "添加任务到延时队列里面:" + orderId);
+        delayedQueue.offer(orderId, delay, timeUnit);
+    }
+
 }
