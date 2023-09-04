3cf17cf0459ac3d77b6ac77c7daed10ca3803696
steve <934415751@qq.com>
2023-09-04 14:20:29 +0800

    add 优化布隆过滤器

--- src/main/java/cn/chahuyun/economy/redis/RedisUtils.java
+++ src/main/java/cn/chahuyun/economy/redis/RedisUtils.java
@@ -37,14 +37,15 @@ public class RedisUtils {
         String key = "fish:sign:" + prop + RedisKeyConstant.COLON_SPILT + groupId;
         RBloomFilter<Long> rBloomFilter = RedissonConfig.getRedisson().getBloomFilter(key);
         // 初始化预期插入的数据量为10000和期望误差率为0.01
-        rBloomFilter.tryInit(100, 0.01);
+        rBloomFilter.tryInit(500, 0.01);
         return rBloomFilter;
+
     }
 
-    public static RBloomFilter initOneDayPropBloomFilter(Long groupId, String propKey) {
+    public static RBloomFilter<Long> initOneDayPropBloomFilter(Long groupId, String propKey) {
         String today = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         String key = propKey + today + RedisKeyConstant.COLON_SPILT + groupId;
-        RBloomFilter rBloomFilter = RedissonConfig.getRedisson().getBloomFilter(key);
+        RBloomFilter<Long> rBloomFilter = RedissonConfig.getRedisson().getBloomFilter(key);
         // 初始化预期插入的数据量为10000和期望误差率为0.01
         rBloomFilter.tryInit(100, 0.01);
         rBloomFilter.expire(24, TimeUnit.HOURS);
