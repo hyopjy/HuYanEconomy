384a5ab7e43e3d66b20e6f52e6389b8f38d4caaf
zhangyi <zhangyi@hummerdata.com>
2023-11-16 18:17:03 +0800

    fix 结束bug

--- src/main/java/cn/chahuyun/economy/manager/WorldBossGoalTask.java
+++ src/main/java/cn/chahuyun/economy/manager/WorldBossGoalTask.java
@@ -167,7 +167,7 @@ public class WorldBossGoalTask implements Task {
 
             sb.append("-------").append("\r\n");
 
-            Message messgae = new PlainText("\uD83E\uDD96Boss战结束，钓鱼佬狂砍" + userFishSize + "斤，战况如下：：\r\n");
+            Message messgae = new PlainText("\uD83E\uDD96Boss战结束，钓鱼佬狂砍" + (userFishSize + otherFish) + "斤，战况如下：：\r\n");
             messgae = messgae.plus(sb.toString());
             Objects.requireNonNull(bot.getGroup(groupId)).sendMessage(messgae);
         }
