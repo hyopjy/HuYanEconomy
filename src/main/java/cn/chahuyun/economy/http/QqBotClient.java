288b6b466804ed288b97be6abea178516263a75d
steve <934415751@qq.com>
2024-05-15 18:30:08 +0800

    1.Forest

--- src/main/java/cn/chahuyun/economy/http/QqBotClient.java
+++ src/main/java/cn/chahuyun/economy/http/QqBotClient.java
@@ -1,10 +1,12 @@
 package cn.chahuyun.economy.http;
 
+import com.dtflys.forest.annotation.Address;
 import com.dtflys.forest.annotation.Request;
-
+// 整个接口下的所有方法请求都默认绑定该根地址
+@Address(host = "127.0.0.1", port = "18620")
 public interface QqBotClient {
     // https://forest.dtflyx.com/pages/1.5.36/build_interface/
     // https://gitee.com/igingo/graceful-response
-    @Request("http://localhost:8080/hello")
+    @Request("/hello")
     String simpleRequest();
 }
---
+++ src/main/java/cn/chahuyun/economy/manager/QqBotClientManager.java
@@ -0,0 +1,20 @@
+package cn.chahuyun.economy.manager;
+import cn.chahuyun.economy.http.QqBotClient;
+import com.dtflys.forest.Forest;
+import com.dtflys.forest.config.ForestConfiguration;
+
+public class QqBotClientManager {
+    public static QqBotClient getQqBotClient(){
+        // 获取 Forest 全局配置对象
+        ForestConfiguration configuration = Forest.config();
+        // 连接池最大连接数
+        configuration.setMaxConnections(1000);
+        // 连接超时时间，单位为毫秒
+        configuration.setConnectTimeout(2000);
+        // 数据读取超时时间，单位为毫秒
+        configuration.setReadTimeout(2000);
+
+        return Forest.client(QqBotClient.class);
+
+    }
+}
