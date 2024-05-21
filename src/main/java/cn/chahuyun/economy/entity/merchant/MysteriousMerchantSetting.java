1338814b7bf68440be3d83e5560a48cd3c5bd294
steve <zhangyi@hummerdata.com>
2024-05-21 18:26:57 +0800

    1.神秘商店 表结构设计

---
+++ src/main/java/cn/chahuyun/economy/entity/merchant/MysteriousMerchantSetting.java
@@ -0,0 +1,54 @@
+package cn.chahuyun.economy.entity.merchant;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * 设置商品数量
+ */
+@Entity(name = "MysteriousMerchantSetting")
+@Table
+@Getter
+@Setter
+public class MysteriousMerchantSetting implements Serializable {
+
+    private static final long serialVersionUID = 6353309591300277475L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long settingId;
+
+    /**
+     * 开始小时数
+     */
+    private Integer hour;
+
+    /**
+     * 开始分钟数
+     */
+    private Integer minute;
+
+    /**
+     * 过多久消失
+     */
+    private Integer passMinute;
+
+    /**
+     * 上架商品列表
+     */
+    private String goodCodeStr;
+
+    /**
+     * 上架商品限制数量
+     */
+    private Integer goodLimit;
+
+    /**
+     * 随机几种商品
+     */
+    private Integer randomGoodCount;
+
+}
