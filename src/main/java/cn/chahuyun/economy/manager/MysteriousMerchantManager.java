c9f2d213d5c59c1a6ecf94f27847fc5b75ffa302
steve <zhangyi@hummerdata.com>
2024-05-21 11:16:09 +0800

    1.神秘商店base

---
+++ src/main/java/cn/chahuyun/economy/manager/MysteriousMerchantManager.java
@@ -0,0 +1,61 @@
+package cn.chahuyun.economy.manager;
+
+import net.mamoe.mirai.event.events.MessageEvent;
+
+/**
+ * 神秘商人管理
+ *
+ */
+public class MysteriousMerchantManager {
+
+    /**
+     * 开启神秘商人
+     *
+     * @param event
+     */
+    public static void open(MessageEvent event){
+    //    开启神秘商人
+    }
+
+    /**
+     * 关闭神秘商人
+     *
+     * @param event
+     */
+    public static void close(MessageEvent event){
+    //    关闭神秘商人
+    }
+
+    /**
+     * 设置神秘商人base
+     *
+     * @param event
+     */
+    public static void setting(MessageEvent event){
+    //    设置神秘商人 14,17,21 15% 10(几分钟消失)  83-92(商品编码范围) 2(几种道具)  1-3(随机道具库存)
+
+    }
+
+    /**
+     * 设置神秘商人
+     *
+     * @param event
+     */
+    public static void buyShop(MessageEvent event){
+        //    神秘商人商店 (excel导入)
+    }
+
+    public static void exchangeShop(MessageEvent event){
+        //    神秘商人兑换商店 (excel导入)
+    }
+
+    public static void buy(MessageEvent event){
+        //    购买(想个新命令)
+    }
+
+    public static void exchange(MessageEvent event){
+        //    兑换(想个新命令)
+    }
+
+    // 道具背包 展示神秘商店标志
+}
