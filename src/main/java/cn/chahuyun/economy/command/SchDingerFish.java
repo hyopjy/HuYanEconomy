1f37ee2c2ea44786a018da5130e6966a586d4d5e
steve <934415751@qq.com>
2024-08-05 15:31:43 +0800

    道具21薛定谔的鱼加一个限制：目标背包内可交易的道具种类少于3种时不获得道具“背包里的东西太少了！啥也没获得”

--- src/main/java/cn/chahuyun/economy/command/SchDingerFish.java
+++ src/main/java/cn/chahuyun/economy/command/SchDingerFish.java
@@ -1,5 +1,6 @@
 package cn.chahuyun.economy.command;
 
+import cn.chahuyun.economy.entity.UserBackpack;
 import cn.chahuyun.economy.entity.UserInfo;
 import cn.chahuyun.economy.entity.props.PropsFishCard;
 import cn.chahuyun.economy.factory.AbstractPropUsage;
@@ -13,9 +14,11 @@ import cn.hutool.core.util.RandomUtil;
 import net.mamoe.mirai.contact.NormalMember;
 import net.mamoe.mirai.message.data.*;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.compress.utils.Lists;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -67,14 +70,26 @@ public class SchDingerFish extends AbstractPropUsage {
         if(CollectionUtils.isEmpty(tradablePropCode)){
             return;
         }
+
+        // 不减去道具
+//        PluginManager.getPropsManager().deleteProp(targetUserInfo, randomPropsFishCard);
+
+        List<UserBackpack> targetPack  = Optional.ofNullable(targetUserInfo.getBackpacks()).orElse(Lists.newArrayList());
+        int count = (int) targetPack.stream().map(UserBackpack::getPropsCode).distinct().count();
+        if (count < 3) {
+            subject.sendMessage(new MessageChainBuilder().append(new QuoteReply(event.getMessage()))
+                    .append(propsCard.getName()).append("使用成功").append("\r\n")
+                    .append(new At(target).getDisplay(group)).append("获得「薛定谔」buff，发送的所有消息都会被撤回")
+                    .append(new At(userInfo.getQq()).getDisplay(group)).append("目标背包里的东西太少了！啥也没获得")
+                    .append("\r\n")
+                    .build());
+            return;
+        }
+
         // 生成一个随机索引
         int randomIndex = RandomUtil.randomInt(tradablePropCode.size());
         // 获取随机元素
         PropsFishCard randomPropsFishCard = tradablePropCode.get(randomIndex);
-        // 不减去道具
-//        PluginManager.getPropsManager().deleteProp(targetUserInfo, randomPropsFishCard);
-
-
         // 发送者获得道具
         PluginManager.getPropsManager().addProp(userInfo, randomPropsFishCard);
 
