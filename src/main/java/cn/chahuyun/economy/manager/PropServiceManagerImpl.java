20c876f51518470239d5ead2e405813c53f6653b
zhangyi <zhangyi@hummerdata.com>
2025-06-24 17:43:57 +0800

    增加订阅

---
+++ src/main/java/cn/chahuyun/economy/manager/PropServiceManagerImpl.java
@@ -0,0 +1,17 @@
+package cn.chahuyun.economy.manager;
+
+import api.PropService;
+
+import java.util.List;
+
+public class PropServiceManagerImpl implements PropService {
+    @Override
+    public boolean addPropToPlayer(Long aLong, long l, String s, int i) {
+        return false;
+    }
+
+    @Override
+    public int addPropToPlayers(Long aLong, List<Long> list, String s, int i) {
+        return 0;
+    }
+}
